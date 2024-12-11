f09a9f6e4bc587200cddf19c43aa5068622e5a35
lukebrevoort <luke@brevoort.com>
2024-12-11 16:49:13 -0500

    Changed front text

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ export default function Main() {
                 <span className="p-bold"> Computer Science!</span> I am passionate about <span className="p-bold">AI and Fullstack Development.</span>
             </p>
 
-            <br /><p>I enjoy <span className="p-bold">Climbing, Hiking, Weightlifting, and Lacrosse</span>, so really anything active :)</p>
+            <br /><p>I enjoy <span className="p-bold">Frisbee, Hiking, Weightlifting, and Lacrosse</span>, so really anything active :)</p>
 
             <br /><p>I also really enjoy exploring <span className="p-bold">NYC</span> and <span className="p-bold">love spending time with my friends!</span></p><br />
 
