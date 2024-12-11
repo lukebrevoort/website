168169e9b1e0ed934552b2a0dd78bf5af553d70b
lukebrevoort <luke@brevoort.com>
2024-12-11 16:23:47 -0500

    changed how images are chose

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,8 +26,8 @@ export default function Main() {
 
 
             <div className='project'>
-                <Project img="./images/Money Minimal Picture.jpg" title="Sentiment Analysis Project"/>
-                <Project  img="./images/MacBook Pro Plant Photo.jpg" title="Personal Website"/>
+                <Project img="./images/sentiment.jpg" title="Sentiment Analysis Project"/>
+                <Project  img="./images/website.png" title="Personal Website"/>
                 <Project img="./images/sql.jpg" title="Football SQL"/>
                 <Project img="./images/calc.png" title="GO Calculator"/>
             </div>
