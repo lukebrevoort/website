d17500ae9302e9aee040efce2a1a2298a4196825
lukebrevoort <luke@brevoort.com>
2025-03-18 22:44:38 -0600

    properly implemented sidebar to blog page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("692af3ec4233c53c")
+/******/ 		__webpack_require__.h = () => ("ea17285cc5f7ff7d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
