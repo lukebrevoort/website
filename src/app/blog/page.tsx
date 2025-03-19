870f828b163b06a363da30653e3a5b2c5be026e9
lukebrevoort <luke@brevoort.com>
2025-03-18 19:41:55 -0600

    removed auth for deprecated extension project

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { lukesFont, crimsonText } from "@/app/fonts"
+
+
+export default function BlogPage() {
+
+
+    return (
+        <div>
+        <h1>Blog Page</h1>
+        </div>
+    )
+}
