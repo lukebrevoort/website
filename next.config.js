280ca7aafc711f996ce9c4d794ab4ea24dd02b60
lukebrevoort <luke@brevoort.com>
2025-03-25 16:40:46 -0400

    adding degut images and updating next.js

--- next.config.js
+++ next.config.js
@@ -3,31 +3,41 @@ const nextConfig = {
     output: 'standalone',
     distDir: '.next',
     images: {
+      domains: [
+        'fonts.googleapis.com',                           // Google Fonts domain
+        'prod-files-secure.s3.us-west-2.amazonaws.com',  // AWS S3 domain
+        'vercel-blob.com',                               // Vercel Blob domain
+        'public.blob.vercel-storage.com',                // Alternative Vercel Blob domain
+        'zah3ozwhv9cp0qic.public.blob.vercel-storage.com' // Your specific blob domain
+      ],
+      // FIXED: Combined all remote patterns into one array
       remotePatterns: [
         {
           protocol: 'https',
           hostname: 'zah3ozwhv9cp0qic.public.blob.vercel-storage.com',
           port: '',
+          pathname: '/**',
         },
-      ],
-      domains: [
-        'fonts.googleapis.com',                          // Google Fonts domain
-        'prod-files-secure.s3.us-west-2.amazonaws.com',  // AWS S3 domain
-        'vercel-blob.com',                               // Vercel Blob domain
-        'public.blob.vercel-storage.com'                 // Alternative Vercel Blob domain
-      ],
-      // Optional: Set a remote pattern for all AWS S3 domains
-      remotePatterns: [
         {
           protocol: 'https',
           hostname: '**.amazonaws.com',
           pathname: '/**',
         },
+        {
+          protocol: 'https',
+          hostname: '**.vercel-blob.com',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.blob.vercel-storage.com',
+          pathname: '/**',
+        }
       ],
     },
-  eslint: {
-    ignoreDuringBuilds: true, // Only if you want to ignore ESLint during builds
-  },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
