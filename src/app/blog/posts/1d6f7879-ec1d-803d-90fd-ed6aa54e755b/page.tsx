af7e911b08160f5d8c1d2bbfe5dde2fe0fdeb1c5
lukebrevoort <luke@brevoort.com>
2025-04-15 15:22:34 +0000

    Update blog post from Notion (Page ID: 1d6f7879-ec1d-803d-90fd-ed6aa54e755b)

--- src/app/blog/posts/1d6f7879-ec1d-803d-90fd-ed6aa54e755b/page.tsx
+++ src/app/blog/posts/1d6f7879-ec1d-803d-90fd-ed6aa54e755b/page.tsx
@@ -31,7 +31,7 @@ First and foremost, Hannah and I decided to redo the Logo! We went for something
 
 While I might not be the best artists, I try my best to make the frontend as **stylish** as possible. My design tool of choice for this was **Figma**, a fantastic and easy to use website design tool I’ve fiddled with in the past. Its very clean and made for developers in mind. After quickly skimming a 30 minute tutorial I felt ready for the artistic challenge 🖼️👨‍🎨
 
-> If you would like to go to the direct page and go through each of my designs just click this [Link](https://www.figma.REDACTED_SECRETate-Design--Community-?node-id=0-1&t=pMSJnbgwloGHKucI-1)!
+> If you would like to go to the direct Figma page and go through each of my designs, click on this [link](https://www.figma.REDACTED_SECRET)!
 
 
 ## The Vision 👁️👄👁️
