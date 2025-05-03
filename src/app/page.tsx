e6a16b0efaf0cab18533ca80a5f15c325fd1b86a
lukebrevoort <luke@brevoort.com>
2025-05-03 10:23:58 -0400

    included speed insights from vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "@/app/dashboard/page";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function Home() {
   return (
     <div>
       <Sidebar />
+      <SpeedInsights />
     </div>
   );
 }
